c87c642f4177a19104a8d989945c1387c567ada6
Brice <brettinb@hotmail.fr>
2025-08-12 14:31:19 +0200

    modif CORS method

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'https://my-portfolio-7byb1ess7-bricebretts-projects.vercel.app/',
-      'http://localhost:3001',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: /vercel\.app$/,
   });
 
   await app.listen(process.env.PORT || 4000, '0.0.0.0');
